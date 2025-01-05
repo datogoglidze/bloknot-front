c8554d0fa6e98c42a417e1150db2c6ebc9e6aee4
David Goglidze <70262653+datogoglidze@users.noreply.github.com>
2025-01-05 19:16:27 +0400

    Remove unnecessary styles (#9)

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
-import "./index.css";
 import App from "./App.jsx";
 import theme from "./theme";
 

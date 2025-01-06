b551566831fcdfa77a580ed33734eaacbd408c1d
Schwifter <d.goglidze@sangu.edu.ge>
2025-01-06 15:41:06 +0400

    Clear vite configuration

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/",
   plugins: [react()],
-  preview: {
-    port: 5173,
-    strictPort: true,
-  },
-  server: {
-    port: 5173,
-    strictPort: true,
-    host: true,
-    origin: "http://localhost:5173",
-  },
 });

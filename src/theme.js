dba06661ed81c62f41a3d8bd19e770753b342cfb
David Goglidze <70262653+datogoglidze@users.noreply.github.com>
2025-01-06 16:10:34 +0400

    Polish for publishing (#12)

--- src/theme.js
+++ src/theme.js
@@ -5,9 +5,6 @@ const theme = createTheme({
     primary: {
       main: "#1976d2",
     },
-    secondary: {
-      main: "#9c27b0",
-    },
   },
   typography: {
     fontFamily: "Roboto, Arial, sans-serif",

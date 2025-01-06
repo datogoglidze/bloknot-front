80086123c484530aa6e4ef4b9413b12cccf0d208
David Goglidze <70262653+datogoglidze@users.noreply.github.com>
2025-01-06 15:09:17 +0400

    Mobile UI (#11)

--- src/theme.js
+++ src/theme.js
@@ -11,12 +11,51 @@ const theme = createTheme({
   },
   typography: {
     fontFamily: "Roboto, Arial, sans-serif",
+    h1: {
+      "@media (max-width:650px)": {
+        fontSize: "2rem",
+      },
+    },
+    h3: {
+      "@media (max-width:650px)": {
+        fontSize: "1.8rem",
+      },
+    },
+    body1: {
+      "@media (max-width:650px)": {
+        fontSize: "0.85rem",
+      },
+    },
   },
   components: {
     MuiTableCell: {
       styleOverrides: {
         root: {
           textAlign: "center",
+          "@media (max-width:650px)": {
+            fontSize: "0.75rem",
+            padding: "10px",
+          },
+        },
+      },
+    },
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          "@media (max-width:650px)": {
+            fontSize: "0.75rem",
+          },
+        },
+      },
+    },
+    MuiTextField: {
+      styleOverrides: {
+        root: {
+          "& .MuiInputBase-input": {
+            "@media (max-width:650px)": {
+              fontSize: "0.875rem",
+            },
+          },
         },
       },
     },
